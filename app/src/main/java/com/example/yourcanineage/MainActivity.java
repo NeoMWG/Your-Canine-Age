f35b0d57e21b93ab9cae2bd4a06dcb8656f5d611
Miguel Chavez <ingenieria.mc01@gmail.com>
2024-07-01 13:25:51 -0500

    Adding Adding the logic to calculate dog age in the main activity

--- app/src/main/java/com/example/yourcanineage/MainActivity.java
+++ app/src/main/java/com/example/yourcanineage/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.yourcanineage;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.yourcanineage.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnCalculateDogAge.setOnClickListener(v -> {
+            String age = binding.etEnterYourAge.getText().toString();
+            if (!age.isEmpty()) {
+                int dogAge = Integer.parseInt(age) * 7;
+                String resultCalcDogAge = getString(R.string.result_dog_age_string, dogAge);
+                binding.tvDogAgeResult.setText(resultCalcDogAge);
+            } else {
+                Toast toast = Toast.makeText(this, R.string.you_must_enter_your_age, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 400);
+                toast.show();
+            }
         });
     }
 }
